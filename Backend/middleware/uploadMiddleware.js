c9604add0a1a1cd1adb4cc04af637552657b11e9
Vishal Kamble <vishalkamble38396@gmail.com>
2026-02-09 12:54:14 +0530

    Initial Commit

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -1,20 +1,3 @@
-// import multer from "multer";
-// import { CloudinaryStorage } from "multer-storage-cloudinary";
-// import cloudinary from "../config/cloudinary.js";
-
-// const storage = new CloudinaryStorage({
-//   cloudinary,
-//   params: {
-//     folder: "leaders",
-//     allowed_formats: ["jpg", "png", "jpeg"],
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// export default upload;
-
-
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
